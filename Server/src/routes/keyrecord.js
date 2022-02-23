aacaf4ba9c14aa51b1f5061b60f4cb29f3dffbdd
luminosuxv <drgvyhn@gmail.com>
2022-02-23 16:27:48 +0900

    스마트키 이력조회에서 해당 키가 없을경우 호출할 때 SQL에러가 나서 예외처리 추가

--- Server/src/routes/keyrecord.js
+++ Server/src/routes/keyrecord.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../database/dbconnection");
 let bodyParser = require("body-parser");
+let cookieParser = require("cookie-parser");
 
+router.use(cookieParser());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,6 +15,7 @@ router.get('/main/view_keyrecord', function(req, res) {
 
     let sql1 = 'select SerialNum, Time, KeyState, GPSLat, GPSLong, Method, Email from KeyRecord where serialNum = ?';
     let sql2 = 'select OwnerID from Key_Authority where SerialNum = ?';
+    let sql3 = 'select * from KeyInfo where SerialNum = ?';
 
     if (req.session.login === undefined) {
         res.status(404).json ({
@@ -21,7 +24,7 @@ router.get('/main/view_keyrecord', function(req, res) {
         })
     }
     else{
-        connection.query(sql2, serialNum, function(err, result2){
+        connection.query(sql3, serialNum, function (err, result3){
             if (err) {
                 console.log(err);
                 res.status(500).json ({
@@ -29,31 +32,48 @@ router.get('/main/view_keyrecord', function(req, res) {
                     'message': 'DB 오류가 발생했습니다.'
                 })
             }
-            else if (result2[0].OwnerID != req.session.login.Email){
-                res.status(401).json ({
-                    'code' : 401,
-                    "message" : '허가 받지 않은 계정입니다.'
+            else if(result3.length === 0){
+                res.status(400).json ({
+                    'code': 404,
+                    'message': '해당 스마트키의 이력이 없습니다.'
                 })
             }
             else{
-                connection.query(sql1, serialNum, function(err, result) {
+                connection.query(sql2, serialNum, function(err, result2){
                     if (err) {
-                        console.log(err)
+                        console.log(err);
                         res.status(500).json ({
                             'code': 500,
                             'message': 'DB 오류가 발생했습니다.'
                         })
                     }
-                    else if (result.length === 0) {
-                        res.status(400).json ({
-                            'code': 404,
-                            'message': '해당 스마트키의 이력이 없습니다.'
+                    else if (result2[0].OwnerID != req.session.login.Email){
+                        res.status(401).json ({
+                            'code' : 401,
+                            "message" : '허가 받지 않은 계정입니다.'
                         })
                     }
                     else{
-                        res.status(200).json ({
-                            'code': 200,
-                            'message': result
+                        connection.query(sql1, serialNum, function(err, result) {
+                            if (err) {
+                                console.log(err)
+                                res.status(500).json ({
+                                    'code': 500,
+                                    'message': 'DB 오류가 발생했습니다.'
+                                })
+                            }
+                            else if (result.length === 0) {
+                                res.status(400).json ({
+                                    'code': 404,
+                                    'message': '해당 스마트키의 이력이 없습니다.'
+                                })
+                            }
+                            else{
+                                res.status(200).json ({
+                                    'code': 200,
+                                    'message': result
+                                })
+                            }
                         })
                     }
                 })
