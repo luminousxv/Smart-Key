a06692123d6ce79e0295eaab858b4a003aaa37d1
luminousxv <66877622+luminousxv@users.noreply.github.com>
2022-03-03 21:33:54 +0900

    보안 수정

--- Server/src/routes/join.js
+++ Server/src/routes/join.js
@@ -21,7 +21,7 @@ const smtpTransport = nodemailer.createTransport({
     secure: true,
     auth : {
         user: "drgvyhn@gmail.com",
-        pass: "wxgfmpwbkywmfnix"
+        pass: "****"
     }
 });
 
