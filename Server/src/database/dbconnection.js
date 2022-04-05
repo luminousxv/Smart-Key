d8126f4db9eb02c0ce226ecfd3e15ca58a81f0ba
luminousxiv <drgvyhn@gmail.com>
2022-04-06 03:41:29 +0900

    db 업데이트로 인한 수정

--- Server/src/database/dbconnection.js
+++ Server/src/database/dbconnection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 //Database configuration
 const connection = mysql.createPool({
-    host: "localhost",
-    user: "Smart-Key",
-    database: "SMART_KEY",
-    password: "1234",
+    host: "127.0.0.1",
+    user: "root",
+    database: "Smart_Key",
+    password: "q1w2e3r4!@",
     port: 3306
 });
 
