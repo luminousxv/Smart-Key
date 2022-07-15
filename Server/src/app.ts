2e847cb5192238b42963302ab1174d90f1b0a9b7
luminousxiv <drgvyhn@gmail.com>
2022-07-15 20:23:04 +0900

    키 이력 API(keyrecord.js), 키 공유 API(keyshare.js), 키 등록 API(register_key.js), 계정비밀번호 초기화 API(resetPW.js) TS로 변환으로 인한 app.ts 등록

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,15 +2,15 @@ import bodyParser from "body-parser";
 import express from "express";
 import joinRouter from "./routes/join";
 import loginRouter from "./routes/login";
-// import resetPwRouter from "./routes/resetPW";
+import resetPwRouter from "./routes/resetPW";
 import keylistRouter from "./routes/keylist";
-// import registerkeyRouter from "./routes/register_key";
+import registerkeyRouter from "./routes/register_key";
 import deletekeyRouter from "./routes/delete_key";
-// import keyrecordRouter from "./routes/keyrecord";
+import keyrecordRouter from "./routes/keyrecord";
 import keycontrolRouter from "./routes/keycontrol";
 import keyPwdRouter from "./routes/keyPW";
 // import rpiRouter from "./routes/rpi_control";
-// import shareRouter from "./routes/keyshare";
+import shareRouter from "./routes/keyshare";
 // import rpiImageRouter from "./routes/rpi_image";
 
 const app = express();
@@ -29,15 +29,15 @@ app.use(
 
 app.use("/Smart-Key", joinRouter);
 app.use("/Smart-Key", loginRouter);
-// app.use("/Smart-Key", resetPwRouter);
+app.use("/Smart-Key", resetPwRouter);
 app.use("/Smart-Key", keylistRouter);
-// app.use("/Smart-Key", registerkeyRouter);
+app.use("/Smart-Key", registerkeyRouter);
 app.use("/Smart-Key", deletekeyRouter);
-// app.use("/Smart-Key", keyrecordRouter);
+app.use("/Smart-Key", keyrecordRouter);
 app.use("/Smart-Key", keycontrolRouter);
 app.use("/Smart-Key", keyPwdRouter);
 // app.use("/Smart-Key", rpiRouter);
-// app.use("/Smart-Key", shareRouter);
+app.use("/Smart-Key", shareRouter);
 // app.use("/Smart-Key", rpiImageRouter);
 
 // Server
