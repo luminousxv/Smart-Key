2d9182e51d0502ea2e701191972ebcc27c64a8f9
luminousxiv <drgvyhn@gmail.com>
2022-07-20 16:04:30 +0900

    KeyRecord, KeyShare, login, Register_Key, resetPW, rpi_Control, rpi_Image API sql문 정의

--- Server/src/modules/sql.ts
+++ Server/src/modules/sql.ts
@@ -32,4 +32,65 @@ const KeyPw = {
   select: "select SmartPwd, Salt from KeyInfo where SerialNum = ?",
 };
 
-export = { Delete, Join, KeyControl, Mode, List, KeyPw };
+const KeyRecord = {
+  select_Record:
+    "select SerialNum, Time, KeyState, GPSLat, GPSLong, Method, Email from KeyRecord where serialNum = ?",
+  select_Authority: "select OwnerID from Key_Authority where SerialNum = ?",
+  select_Image: "select Image from KeyRecord where SerialNum = ? and Time = ?",
+};
+
+const KeyShare = {
+  select_Authority: "select * from Key_Authority where SerialNum = ?",
+  update_KeyInfo:
+    "update KeyInfo set Shared = ?, SharedID = ? where SerialNum = ?",
+  update_Authority: "update Key_Authority set ShareID = ? where SerialNum = ?",
+  select_User: "select * from Users where UserEmail = ?",
+  insert_Record:
+    "insert into KeyRecord (SerialNum, Time, Method, Email) values (?, ?, ?, ?)",
+};
+
+const Register = {
+  select_KeyInfo: "select KeyState from KeyInfo where SerialNum = ?",
+  insert_KeyInfo:
+    "insert into KeyInfo (SerialNum, KeyName, KeyState, UserID, SmartPwd, Salt, Shared, Mode) values (?, ?, ?, ?, ?, ?, ?, ?)",
+  insert_Record:
+    "insert into KeyRecord (SerialNum, Time, KeyState, Method, Email) values (?, ?, ?, ?, ?)",
+  insert_Authority:
+    "insert into Key_Authority(SerialNum, OwnerID) values (?, ?)",
+};
+
+const PWReset = {
+  select:
+    "select * from Users where UserEmail = ? and UserName = ? and UserBirth = ?",
+  update: "update Users set UserPwd = ?, Salt = ? where UserEmail = ?",
+};
+
+const RPIControl = {
+  select: "select KeyState, Mode from KeyInfo where SerialNum = ?",
+  delete_KeyInfo: "delete from KeyInfo where SerialNum = ?",
+  delete_Record: "delete from KeyRecord where SerialNum = ?",
+  delete_Authority: "delete from Key_Authority where SerialNum = ?",
+  update_KeyInfo: "update KeyInfo set KeyState = ? where SerialNum = ?",
+  insert_Record:
+    "insert into KeyRecord (SerialNum, Time, KeyState, Method) values (?, ?, ?, ?)",
+};
+
+const Image = {
+  insert:
+    "insert into KeyRecord (SerialNum, Time, KeyState, Method, Image) values (?, ?, ?, ?, ?)",
+};
+
+export = {
+  Delete,
+  Join,
+  KeyControl,
+  Mode,
+  List,
+  KeyPw,
+  KeyRecord,
+  KeyShare,
+  Register,
+  PWReset,
+  RPIControl,
+  Image,
+};
